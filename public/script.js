a2b2cd8810de7c493785ccceeacd4fda3f0cc888
Alexander Raschid <alexraschid1337@gmail.com>
2019-01-12 18:33:55 +0000

    Managed to get my name from firebase

--- public/script.js
+++ public/script.js
@@ -6,3 +6,43 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+var config = {
+    apiKey: "AIzaSyD9uZozo2SnpUaazbEcqEuPKCsi-apX8x0",
+    authDomain: "my-portfolio-6d3ae.firebaseapp.com",
+    databaseURL: "https://my-portfolio-6d3ae.firebaseio.com",
+    projectId: "my-portfolio-6d3ae",
+    storageBucket: "my-portfolio-6d3ae.appspot.com",
+    messagingSenderId: "282524712234"
+};
+firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+
+
+var firstName = database.ref("first-name");
+var lastName = database.ref("last-name");
+
+console.log(database);
+
+firstName.on('value',function(snapshot){
+    
+    
+    
+    var name = snapshot.val();
+    $('.fName').html(name);
+    
+});
+
+
+lastName.on('value',function(snapshot){
+    var name = snapshot.val();
+    $(".lName").html(name)
+});
+
+
+
+$(document).ready(function(){
+
+});
